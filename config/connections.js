efc7bd85d29004ed4247ff9a82ad72c55458ac87
arman1823 <a.g.aleksanyan@gmail.com>
2015-07-13 17:32:57 +0400

    first commit

---
+++ config/connections.js
@@ -0,0 +1,9 @@
+module.exports.connections = {
+    mysql: {
+        module: 'sails-mysql',
+        host: 'localhost',
+        user: 'root',
+        password: 'vayeshtras3000',
+        database: 'event_consort'
+    }
+};
